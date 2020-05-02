82f9c16ee24402a5bef36834b311b7e89d7d61f8
unknown <you@example.com>
2020-05-02 06:01:16 -0300

    Change background change code

--- js/home.js
+++ js/home.js
@@ -1,73 +1,45 @@
-/* ===============================
-   INÍCIO - Carregamento da página
-=============================== */
+function initialAnimation() {
+  const content = document.querySelector('.container')
+  const { body } = document
 
-function carrega() {
-  document.body.style.display = 'none'
-  document.body.style.backgroundImage = 'none'
-  document.body.style.transform = 'translate(0, 20%)'
-  document.body.style.overflow = 'hidden'
-  document.body.style.opacity = 0
+  content.style.transform = 'translate(0, 20%)'
+  body.style.overflow = 'hidden'
 
   setTimeout(function () {
-    document.body.style.display = 'block'
-    document.body.style.backgroundImage = 'url(img/planos-de-fundo/fundo1.jpg)'
-  }, 2500)
+    content.style.transform = 'translate(0, 0)'
+    content.style.opacity = 1
+  }, 2000)
 
   setTimeout(function () {
-    document.body.style.transform = 'translate(0, 0)'
-    document.body.style.opacity = 1
-  }, 5000)
-
-  setTimeout(function () {
-    document.body.style.overflow = 'auto'
-  }, 7500)
+    body.style.overflow = 'auto'
+  }, 4000)
 }
 
-/* ===============================
-   FIM - Carregamento da página
-=============================== */
+function changeBackground() {
+  const backgrounds = [
+    'img/planos-de-fundo/fundo1.jpg',
+    'img/planos-de-fundo/fundo2.jpg',
+    'img/planos-de-fundo/fundo3.jpg',
+    'img/planos-de-fundo/fundo4.jpg',
+    'img/planos-de-fundo/fundo5.jpg',
+    'img/planos-de-fundo/fundo6.jpg',
+  ]
+  let currentIndex = 0
 
-/* ===================================
-   INÍCIO - Mudança da imagem de fundo
-=================================== */
+  setInterval(function () {
+    const randomIndex = Math.round(Math.random() * (backgrounds.length - 1))
+    const nextIndex =
+      currentIndex === randomIndex
+        ? randomIndex + 1 > backgrounds.length
+          ? 0
+          : randomIndex + 1
+        : randomIndex
 
-/*endereços das imagens*/
-var background_url = [
-  'img/planos-de-fundo/fundo2.jpg',
-  'img/planos-de-fundo/fundo3.jpg',
-  'img/planos-de-fundo/fundo4.jpg',
-  'img/planos-de-fundo/fundo6.jpg',
-  'img/planos-de-fundo/fundo1.jpg',
-]
+    document.body.style.backgroundImage = `url(${backgrounds[currentIndex]})`
 
-/*pré-carrega imagens*/
-for (var i = 0; i < background_url.length; i++) {
-  new Image().src = background_url[i]
+    currentIndex = nextIndex
+  }, 6000)
 }
 
-/*mudança do background*/
-function mudaBackground(indice) {
-  setTimeout(function () {
-    document.body.style.backgroundImage = 'url(' + background_url[indice] + ')'
-    if (indice + 1 >= background_url.length) {
-      mudaBackground(0)
-    } else {
-      mudaBackground(indice + 1)
-    }
-  }, 10000)
-}
-
-/* ===================================
-   FIM - Mudança da imagem de fundo
-=================================== */
-
-/* ==================
-   Chamada de funções
-================== */
-
-carrega()
-
-setTimeout(function () {
-  mudaBackground(0)
-}, 7500)
+initialAnimation()
+changeBackground()

671f4bc1cba93df4f6a9e82670eaa9dde122ac53
unknown <you@example.com>
2020-05-02 05:24:06 -0300

    Prettier

--- js/paginas.js
+++ js/paginas.js
@@ -2,9 +2,9 @@
    INÍCIO - Menu responsivo
 ======================== */
 
-document.getElementById('barra-navegacao-toggle').onclick = function() {
-  document.getElementById('barra-navegacao').classList.toggle('ativa');
-};
+document.getElementById('barra-navegacao-toggle').onclick = function () {
+  document.getElementById('barra-navegacao').classList.toggle('ativa')
+}
 
 /* ========================
    FIM - Menu responsivo
